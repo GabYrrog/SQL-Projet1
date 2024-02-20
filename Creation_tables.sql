fc174c7d3c2cb158d88e1b906818cfc1cb84ae45
Arzyel <emeric_rivard@hotmail.com>
2024-02-20 12:12:19 -0500

    update tables

--- Creation_tables.sql
+++ Creation_tables.sql
@@ -1,7 +1,8 @@
 -- Emeric drop tables
 DROP TABLE IF EXISTS Items;
 DROP TABLE IF EXISTS Avatar;
-DROP TABLE IF EXISTS PaireItemsEtMonde;
+DROP TABLE IF EXISTS Items_monde;
+DROP TABLE IF EXISTS Phrases;
 
 -- Emeric création table Items
 CREATE TABLE Items (
@@ -15,7 +16,33 @@ CREATE TABLE Items (
 -- Emeric création table Avatar
 
 CREATE TABLE Avatar (
-
+    id_nom              VARCHAR(32) PRIMARY KEY,
+    couleur_1
+    couleur_2
+    couleur_3            
+    date_creation       DATE DEFAULT CURRENT_DATE NOT NULL,
+    mox                 INTEGER DEFAULT 0 NOT NULL CHECK(mox BETWEEN -1000000000 AND 1000000000),
+    alias_joueur        VARCHAR(32) NOT NULL            
 );
 
--- Emeric création table PaireItemsEtMonde
+-- Emeric création table Items_monde
+
+CREATE TABLE Items_monde (
+    id_items_monde      SERIAL PRIMARY KEY,
+    monde               VARCHAR(16) NOT NULL,
+    item                INTEGER NOT NULL
+);
+
+-- Emeric création table Phrases
+CREATE TABLE Phrases (
+    id_phrase           SERIAL PRIMARY KEY,
+    id_avatar           VARCHAR(32) NOT NULL,
+    phrase              VARCHAR(64) NOT NULL
+);
+
+
+-- ALTER TABLE pour foreign keys emeric
+ALTER TABLE Avatar ADD FOREIGN KEY (alias_joueur) REFERENCES Joueur (alias);
+ALTER TABLE Items_monde ADD FOREIGN KEY (monde) REFERENCES Monde (nom);
+ALTER TABLE Items_monde ADD FOREIGN KEY (item) REFERENCES Items (id_items_monde);
+ALTER TABLE Phrases ADD FOREIGN KEY (id_avatar) REFERENCES Avatar (id_nom);
