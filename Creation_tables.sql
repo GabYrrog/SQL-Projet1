bfd4b14464699293e7013193506c3ef582cc34cc
Vincent Fournier <genesisqc@hotmail.ca>
2024-02-20 12:13:14 -0500

    little changes

--- Creation_tables.sql
+++ Creation_tables.sql
@@ -4,10 +4,10 @@ CREATE TABLE Activite(
 	debut 		TIMESTAMP 	NOT NULL,
 	fin 		TIMESTAMP 	NOT NULL,
 	duree 		INT 		NOT NULL,
-	nom_joueur	VARCHAR(40)	NOT NULL,
-)
+	nom_joueur	VARCHAR(40)	NOT NULL
+);
 
-ALTER TABLE activite ADD CONSTRAINT fk_nom_joueur FOREIGN KEY (joueur) REFERENCES Joueur(id_alias)
+ALTER TABLE activite ADD CONSTRAINT fk_nom_joueur FOREIGN KEY (joueur) REFERENCES Joueur(id_alias);
 
 --Table Capsule
 CREATE TABLE Capsule(
@@ -16,7 +16,7 @@ CREATE TABLE Capsule(
 	monde		VARCHAR(16)		NOT NULL,
 	avatar		VARCHAR(32) 	NOT NULL,
 	activite	INT				NOT NULL
-)
+);
 
 ALTER TABLE capsule 
 	ADD CONSTRAINT fk_monde FOREIGN KEY (monde) REFERENCES Monde(nom),
@@ -28,9 +28,8 @@ CREATE TABLE Items_avatar(
 	id_item_avatar	SERIAL		PRIMARY KEY,
 	avatar			VARCHAR(32)	NOT NULL,
 	item			INT			NOT NULL
-)
+);
 
 ALTER TABLE item_avatar
 	ADD CONSTRAINT fk_avatar FOREIGN KEY (avatar) REFERENCES Avatar(nom),
-	ADD CONSTRAINT fk_item	FOREIGN KEY (item) REFERENCES Items(id_item)
-	
+	ADD CONSTRAINT fk_item	FOREIGN KEY (item) REFERENCES Items(id_item);
