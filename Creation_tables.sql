09374b1b736b71074afb92664199505a1614ad66
Gabriel <gabriel.gorry@hotmail.ca>
2024-02-22 14:45:19 -0500

    probablitete

--- Creation_tables.sql
+++ Creation_tables.sql
@@ -29,7 +29,7 @@ CREATE TABLE Items (
     id_item             SERIAL PRIMARY KEY,
     nom                 VARCHAR(32) NOT NULL UNIQUE,
     sigle               CHAR(4) NOT NULL UNIQUE CHECK(sigle LIKE 'I%'),
-    probablite          NUMERIC(4,3) DEFAULT 0.025 NOT NULL CHECK (probablite >= 0.000 AND probabilite <= 1.000),
+    probabilite          NUMERIC(4,3) DEFAULT 0.025 NOT NULL CHECK (probabilite >= 0.000 AND probabilite <= 1.000),
     description         VARCHAR(1024) NULL
 );
 
