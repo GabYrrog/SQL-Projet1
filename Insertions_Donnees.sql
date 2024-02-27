d8101f0dbe1e1621eae2926edaed7fc2b5ece6be
Gabriel <gabriel.gorry@hotmail.ca>
2024-02-27 12:20:31 -0500

    ajout emeric

--- Insertions_Donnees.sql
+++ Insertions_Donnees.sql
@@ -52,3 +52,38 @@ VALUES ('Chuck','2');
 INSERT INTO Avatar_habilete (id_nom, id_habilete)
 VALUES ('Chuck','3');
 -- VALIDER si les serial fittent avec mes habiletes
+
+
+-- Emeric
+-- NOTE voir Table Avatar alias_joueur et Table Joueur id_nom n'ont pas le même nombre dans le varchar
+
+-- Emeric insert joueur Emeric
+INSERT INTO Joueur
+        VALUES  ('Emeric','emeric_rivard@hotmail.com','JeffreyBezosTM','h', DEFAULT, '1997-01-28');
+
+-- Emeric insert 1 avatar changer le color value
+INSERT INTO Avatar
+        VALUES  ('Jeff',10,DEFAULT, DEFAULT, 'Emeric');
+
+-- Emeric insert table monde de FreeZoneXTM
+INSERT INTO Monde 
+        VALUES  ('FreeZoneXTM', 'JFZXTM', 'This is the world mister bezos wants to make');
+
+-- Emeric insert 3 capsules d'activité
+INSERT INTO Activite (debut, fin, duree, nom_joueur)
+        VALUES  (
+            now(),
+            now() + INTERVAL '555 minutes',
+            555,
+            'Emeric');
+
+INSERT INTO Capsule(duree,monde,avatar,activite)
+        VALUES  (300, 'FreeZoneXTM', 'Jeff',1);
+
+
+INSERT INTO Capsule(duree,monde,avatar,activite)
+        VALUES  (200, 'FreeZoneXTM', 'Jeff',1);
+
+
+INSERT INTO Capsule(duree,monde,avatar,activite)
+        VALUES  (55, 'FreeZoneXTM', 'Jeff',1);
